3a5469faad95f79657cd7c5a0ae38e29e5461236
dreipeso <claus.schewe@gmx.de>
2021-03-07 08:36:35 +0100

    Added artefact to collect content of all shell history files of all users on a local mac.

--- artefact/localhost/mac/file.py
+++ artefact/localhost/mac/file.py
@@ -1,22 +1,39 @@
+import logging
+import os
+from collections import namedtuple
 from os import path
-from pathlib import Path
 
 from baseclasses.artefact import ArtefactBase
 from source.localhost import expandfilepath
 
 
-class FileContent(ArtefactBase):
-    FILE_PATH_PARAMETER = "filepath"
+TermianlHistory = namedtuple('TerminalHistory', ['Path', 'Content'])
+
+
+class BashHistoryOfAllUsers(ArtefactBase):
+    _logger = logging.getLogger(__name__)
 
     def __init__(self, *args, **kwargs):
         ArtefactBase.__init__(self, *args, **kwargs)
 
+    def __str__(self):
+        result = ''
+        for data in self._collecteddata:
+            result = result + "{}START{}\nPATH: {}\n\nCONTENT:\n{}\n{} END {}".format('*' * 20, '*' * 20,
+                                                                                      data.Path, data.Content,
+                                                                                      '+' * 20, '+' * 20)
+        return result
+
     def collect(self):
-        filepath = self._parameters[self.FILE_PATH_PARAMETER].nodeValue
-        filepath = expandfilepath(filepath)
-        if path.exists(filepath):
-            with open(filepath) as filetoload:
-                self._collecteddata = filetoload.read()
-        else:
-            self._collecteddata = "File '{}' does not exist.".format(
-                self._parameters[self.FILE_PATH_PARAMETER].nodeValue)
+        userfolders = [f.path for f in os.scandir('/Users') if f.is_dir()]
+        self._collecteddata = list(BashHistoryOfAllUsers._collect_bash_history(userfolders))
+
+    @staticmethod
+    def _collect_bash_history(userfolders):
+        historyfilepaths = [path.join(folder, history) for folder in userfolders
+                            for history in ['.bash_history', '.zsh_history']]
+        for historyfile in historyfilepaths:
+            if path.exists(historyfile):
+                BashHistoryOfAllUsers._logger.debug("Found terminal history file '{}'.".format(historyfile))
+                with open(historyfile, encoding='unicode_escape') as hf:
+                    yield TermianlHistory(Path=historyfile, Content=hf.read())
