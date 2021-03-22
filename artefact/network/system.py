d7ba2cdffa7eca02a07f21bdfaa15c77c26c0c98
dreipeso <claus.schewe@gmx.de>
2021-03-22 15:16:36 +0100

    Added support for iterable collection data.

--- artefact/network/system.py
+++ artefact/network/system.py
@@ -2,12 +2,17 @@
 mosk network module for classes collecting system information through a network.
 """
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = '3Peso'
 __all__ = ['TimeFromNTPServer']
 
+import collections
+
 from businesslogic.support import get_time, DEFAULT_TIME_SERVER
 from baseclasses.artefact import ArtefactBase
+
+
+NTPTime = collections.namedtuple('NTPTime', 'Time NTPServer')
 
 
 class TimeFromNTPServer(ArtefactBase):
@@ -23,14 +28,9 @@ class TimeFromNTPServer(ArtefactBase):
         self._title = 'TimeFromNTPServer'
         self._collectionmethod = 'Network call using socket connect.'
         self._description = 'Retrieves the time from a time server. Can be used as reference time.\r\n' \
-                             'Requires network connection.'
-
-    def __str__(self):
-        result = "Time server: '{}' Collected Time: {}".format(self.__timeServer, self.data.collecteddata)
-        result = self.data.get_metadata_as_str(result)
-        return result
+                            'Requires network connection.'
 
     def collect(self):
         self.__timeServer = self.get_parameter(self.TIMER_SERVER_ATTRIBUTE)
-        time = get_time(ntpserver=self.__timeServer)
+        time = NTPTime(Time=get_time(ntpserver=self.__timeServer), NTPServer=self.__timeServer)
         self.data = time
