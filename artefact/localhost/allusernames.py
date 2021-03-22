264f2be8e4a755e6dc6520b28aa95574a27ef852
dreipeso <claus.schewe@gmx.de>
2021-03-22 15:10:12 +0100

    Added support for iterable collection data.

--- artefact/localhost/allusernames.py
+++ artefact/localhost/allusernames.py
@@ -2,7 +2,7 @@
 mosk localhost module for classes collecting information about all users.
 """
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = '3Peso'
 __all__ = ['AllUsernames']
 
@@ -24,20 +24,6 @@ class AllUsernames(ArtefactBase):
         # inside the parameters variable
         self.__properties = [item for item in kwargs['parameters']['properties'].split(',')]
 
-    # TODO Refactor
-    def __str__(self):
-        result = ''
-        for item in self.data.collecteddata:
-            itemasstring = ''
-            for prop in item:
-                itemasstring += "{}\r\n".format(prop)
-            result += "{}\r\n".format(itemasstring)
-        result = self.data.get_metadata_as_str(result)
-
-        return result
-
     def collect(self):
-        result = []
         for pw in getpwall():
-            result.append(["{}: {}".format(prop, getattr(pw, prop)) for prop in self.__properties])
-        self.data = result
+            self.data = ["{}: {}".format(prop, getattr(pw, prop)) for prop in self.__properties]
