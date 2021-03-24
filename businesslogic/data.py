e6cedb5b41ab8ddfe1fc7e0d687d7b0492826499
dreipeso <claus.schewe@gmx.de>
2021-03-24 14:15:14 +0100

    Added ability to log collector name and collector parameters.

--- businesslogic/data.py
+++ businesslogic/data.py
@@ -2,7 +2,7 @@
 Collection data module
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = '3Peso'
 __all__ = ['CollectionData']
 
@@ -17,11 +17,13 @@ class CollectionData:
     CollectionData is the data container which stores the collected data in memory, and additionally required metadata
     as the path of the source file, if there is one,the colelction timestamp, and/or the MD5 hash of the collected data.
     """
-    def __init__(self, data, currentdatetime=None):
+    def __init__(self, data, currentdatetime=None, collector_name=None, collector_parameters=None):
         self.collecteddata = data
         self.currentdatetime = currentdatetime
         self._sourcehash = None
         self._sourcepath = None
+        self._collector_name = collector_name
+        self._collector_parameters = collector_parameters
 
     # TODO Rework metadata formatting
     def __str__(self):
@@ -29,6 +31,7 @@ class CollectionData:
         result += "{}".format(self.collecteddata)
         result += '\r\n\r\n-- Collection Metadata {}'.format('-' * 10)
         result = self.get_metadata_as_str(result)
+        result = self.get_collector_info_as_str(result)
         result += '\r\n\r\n{}\r\n'.format('-' * 33)
         return result
 
@@ -41,6 +44,20 @@ class CollectionData:
             prepend += "\r\nSource MD5: {}".format(self.sourcehash)
         if self.sourcepath is not None:
             prepend += "\r\nSource path: {}".format(self.sourcepath)
+
+        return prepend
+
+    def get_collector_info_as_str(self, prepend=''):
+        if self._collector_name is None:
+            raise ValueError('You must provide a collector name.')
+
+        if prepend != '':
+            prepend = "{}\r\n".format(prepend)
+        if self._collector_name is not None:
+            prepend += "\r\nCollector: {}".format(self._collector_name)
+        if self._collector_parameters is not None:
+            for param in self._collector_parameters.keys():
+                prepend += "\r\n{}: '{}'".format(param, self._collector_parameters[param])
 
         return prepend
 
---
+++ tests/test_data.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from businesslogic.data import CollectionData
+
+
+class TestCollectionData(TestCase):
+
+    def test_get_collector_info_as_str(self):
+        """
+        :return: Should return a string with collector informormation prepended.
+        """
+        actual_parameters = {'Param1': 12, 'Param2': 'Test'}
+        actual_data = CollectionData(data="Test data", collector_name="TestCollector",
+                                     collector_parameters=actual_parameters)
+        teststring = "Test String"
+        expected = "Test String\r\n\r\nCollector: TestCollector\r\nParam1: '12'\r\nParam2: 'Test'"
+        actual = actual_data.get_collector_info_as_str(teststring)
+
+        assert actual == expected
+
+    def test_get_collector_info_as_str_no_params(self):
+        """
+        :return: should only prepend the collector name
+        """
+        actual_data = CollectionData(data="Test data", collector_name="TestCollector")
+        teststring = "Test String"
+        expected = "Test String\r\n\r\nCollector: TestCollector"
+        actual = actual_data.get_collector_info_as_str(teststring)
+
+        assert actual == expected
+
+    def test_get_collector_info_as_str_no_collector_name(self):
+        """
+        With no collector provided it should raise a ValueError
+        """
+        actual_parameters = {'Param1': 12, 'Param2': 'Test'}
+        actual_data = CollectionData(data="Test data", collector_parameters=actual_parameters)
+        teststring = "Test String"
+
+        with self.assertRaises(ValueError):
+            actual_data.get_collector_info_as_str(teststring)
