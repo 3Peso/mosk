c4246bb2449868a8410aa9a426fb2d5b771788a5
dreipeso <claus.schewe@gmx.de>
2021-06-05 08:38:25 +0200

    Test refactoring. Added tests for _prepare_protocol_entry and _get_header_seperator.

--- tests/test_logfileprotocol.py
+++ tests/test_logfileprotocol.py
@@ -5,7 +5,7 @@ import os
 import re
 
 
-class TestLogFileProtocol(TestCase):
+class TestLogFileProtocolFilenameCounter(TestCase):
     def tearDown(self) -> None:
         filepattern = '^\d\d\d\d\d_tst_\d\d\d\d-\d\d-\d\d\.txt$'
         for f in os.listdir('.'):
@@ -29,6 +29,8 @@ class TestLogFileProtocol(TestCase):
 
         self.assertEqual(expected_log_file_name, actual_log_file_protocol_writer.protocol_filename)
 
+
+class TestLogFileProtocolGetCurrentFileCounter(TestCase):
     @patch('protocol.logfileprotocol.glob')
     @patch('businesslogic.placeholders.Placeholder._initialize_global_placeholders')
     def test_get_current_file_counter(self, placeholder_mock, glob_mock):
@@ -53,3 +55,54 @@ class TestLogFileProtocol(TestCase):
         glob_mock.return_value = ['99999_tst_2020-05-26.txt']
 
         self.assertRaises(ValueError, LogFileProtocol._get_current_file_counter, '*')
+
+
+class TestLogFileProtocolPrepareProtocolEntry(TestCase):
+    def test__prepare_protocol_entry_with_header_and_data(self):
+        """
+        Should write entry into file looking like this:
+        ***************************
+        This is another test header
+        ***************************
+        Data
+        """
+        from protocol.logfileprotocol import LogFileProtocol
+        actual_header = "This is another test header"
+        actual_data = "Data"
+        expected_entr = "***************************\r\n" \
+                        "This is another test header\r\n" \
+                        "***************************\r\n" \
+                        "Data\r\n"
+        actual_entry = LogFileProtocol._prepare_protocol_entry(entryheader=actual_header, entrydata=actual_data)
+
+        self.assertEqual(expected_entr, actual_entry)
+
+    def test__prepare_protocol_entry_with_header(self):
+        """
+        Should write entry into file looking like this:
+        ***************************
+        This is another test header
+        ***************************
+        """
+        from protocol.logfileprotocol import LogFileProtocol
+        actual_header = "This is another test header"
+        expected_entr = "***************************\r\n" \
+                        "This is another test header\r\n" \
+                        "***************************\r\n"
+        actual_entry = LogFileProtocol._prepare_protocol_entry(entryheader=actual_header, entrydata=None)
+
+        self.assertEqual(expected_entr, actual_entry)
+
+
+class TestLogFileProtocolGetHeaderSeperator(TestCase):
+    def test__get_header_seperator(self):
+        """
+        Should return a string with as many '*' as the header is long
+        """
+        from protocol.logfileprotocol import LogFileProtocol
+
+        actual_data = "This is a test header"
+        expected_seperator = '*' * len(actual_data)
+        actual_seperator = LogFileProtocol._get_header_seperator(actual_data)
+
+        self.assertEqual(expected_seperator, actual_seperator)
