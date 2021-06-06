f7224a0443cd06f74cfbff5c9116fed18a9071dc
dreipeso <claus.schewe@gmx.de>
2021-06-06 18:05:29 +0200

    Minor test refactoring.

--- tests/test_mosk.py
+++ tests/test_mosk.py
@@ -12,6 +12,6 @@ class TestMosk(TestCase):
     def test_is_globalplaceholder_valid_filexists(self):
         """Should return true"""
         from mosk import is_globalplaceholder_valid
-        filepath = "test.txt"
+        filepath = "./testfiles/test.txt"
 
         self.assertTrue(is_globalplaceholder_valid(filepath))
