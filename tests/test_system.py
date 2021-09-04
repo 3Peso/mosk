70df780df4b898d1b07958be28867c804abda483
dreipeso <claus.schewe@gmx.de>
2021-09-04 08:00:51 +0200

    Prepared unit tests for _supportedsystem

---
+++ tests/test_system.py
@@ -0,0 +1,73 @@
+from unittest import TestCase
+
+
+class TestNVRAMCollectorSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestLocalTimeSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestDetectFusionDriveSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestDetectFileFaultSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestDetectFileByNameSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestInstalledApplicationsSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestFileSystemInformationSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
+
+
+class TestHardwareInformationSupportedSystem(TestCase):
+    def test__supportedsystem(self):
+        """
+        Should be "Darwin"
+        :return:
+        """
+        self.fail()
