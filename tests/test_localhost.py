7c26cd5a45a086b5b7c326e9112b9528911e1997
dreipeso <claus.schewe@gmx.de>
2021-09-19 08:05:34 +0200

    Test refactoring and added a unittest.

--- tests/test_localhost.py
+++ tests/test_localhost.py
@@ -1,23 +1,20 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
 class TestSourceLocalhostExpandFilepath(TestCase):
-    @patch('source.localhost.Path.home')
-    @patch('businesslogic.placeholders.Placeholder._initialize_global_placeholders')
-    def test_expandfilepath_home_in_path(self, placeholder_mock, home_mock):
+    @patch('source.localhost.Path.home', MagicMock(return_value='/home/someuser'))
+    def test_expandfilepath_home_in_path(self):
         """
         Should expand '~' to the home path of the current user.
         """
         import source.localhost
-        home_mock.return_value = '/home/someuser'
         actual_expanded_path = source.localhost.expandfilepath('~/test')
         expected_path = '/home/someuser/test'
 
         self.assertEqual(expected_path, actual_expanded_path)
 
-    @patch('businesslogic.placeholders.Placeholder._initialize_global_placeholders')
-    def test_expandfilepath_no_home_in_path(self, placeholder_mock):
+    def test_expandfilepath_no_home_in_path(self):
         """
         Should do nothing if there is no '~' in the path.
         """
@@ -26,3 +23,16 @@ class TestSourceLocalhostExpandFilepath(TestCase):
         expected_path = '/somepath/test'
 
         self.assertEqual(expected_path, actual_expanded_path)
+
+    def test_expandfilepath_filepath_starts_with_slash(self):
+        """
+        Should expand '~' to the homepath of the current user.
+        :return:
+        """
+        import source.localhost
+        expected_path = '/home/somepath/test'
+        with patch('source.localhost.Path.home', MagicMock(return_value="/home/somepath")):
+            actual_path = source.localhost.expandfilepath('/~/test')
+
+        self.assertEqual(expected_path, actual_path)
+
