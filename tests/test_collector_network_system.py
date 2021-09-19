fb9221b13d2be15df530d7d246caaa2231d514c7
dreipeso <claus.schewe@gmx.de>
2021-09-19 12:42:33 +0200

    Moved some ntp tests into my local integration tests.

--- tests/test_collector_network_system.py
+++ tests/test_collector_network_system.py
@@ -6,29 +6,6 @@ from artefact.network.system import TimeFromNTPServer
 
 
 class TestTimeFromNTPServerCollect(TestCase):
-    def test__collect(self):
-        """
-        Data should be initialized with NTPTime object.
-        :return:
-        """
-        actual_collector = TimeFromNTPServer(parameters={'timeserver': '0.de.pool.ntp.org'}, parent=None)
-
-        actual_collector._collect()
-
-        self.assertIsInstance(actual_collector.data[0].collecteddata, str)
-
-    def test__collect_no_ntp_server(self):
-        """
-        Should use default ntp server.
-        :return:
-        """
-        expected_ntp_server = '0.de.pool.ntp.org'
-        actual_collector = TimeFromNTPServer(parameters={}, parent=None)
-
-        actual_collector._collect()
-
-        self.assertEqual(actual_collector.timeserver, expected_ntp_server)
-
     def test__collect_invalid_ntp_server_address(self):
         """
         Should give back data object with the message informing about a runtime exception.
