335a023be8f583fdd80f556a67e6718ef5d689a3
dreipeso <claus.schewe@gmx.de>
2021-11-05 13:10:29 +0100

    Implemented ToolClass which provides the attribute tool_path to a class which derives from it.

--- artefact/localhost/tools.py
+++ artefact/localhost/tools.py
@@ -4,13 +4,11 @@ mosk localhost module for classes collecting information from output of platform
 
 __author__ = '3Peso'
 
-import logging
 from os import path
 
 from baseclasses.artefact import MacArtefact
 from businesslogic.support import run_terminal_command
-from businesslogic.support import validate_file_signature
-from businesslogic.errors import SignatureMatchError, CollectorParameterError
+from businesslogic.errors import CollectorParameterError
 from baseclasses.artefact import FileClass, ToolClass
 
 
@@ -38,23 +36,3 @@ class PLUtil(MacArtefact, FileClass, ToolClass):
         if plutil_path == 'plutil':
             self.data = \
                 "WARNING: No own copy of 'PLUtil' provided. 'PLUtil' of the live artefact has been used."
-
-    #@property
-    #def tool_path(self):
-    #    logger = logging.getLogger(__name__)
-    #    if path.exists(self._tool_path):
-    #        return self._tool_path
-    #    else:
-    #        logger.warning("Using 'plutil' from artefact.")
-    #        return 'plutil'
-
-    #@tool_path.setter
-    #def tool_path(self, value):
-    #    logger = logging.getLogger(__name__)
-    #    if path.exists(value):
-    #        if validate_file_signature(value):
-    #            self._tool_path = value
-    #        else:
-    #            raise SignatureMatchError(f"The provided PLUtil at '{value}' does not match its signature.")
-    #    else:
-    #        logger.warning(f"Provided tool path '{value}' does not exist.")
